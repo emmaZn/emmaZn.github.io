8fb3b6ce730c629a6cb0114d92aa1553b953823c
Emmanuelle Zenou <emmanuelle.zenou.rogala@gmail.com>
2020-10-17 11:18:35 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <div id="fullpage">
+      <div class="section" id="section1">
+        <div id="canvas">
+        <img class="cloud2" src="./assets/cloud2.png" alt="little cloud moving" />
+        </div><v-row justify="center" class="pt-16">
+          <v-col cols="11" lg="9" align="left" class="mt-16 pt-16">
+            <h1 class="mb-2">
+              Emmanuelle <br />
+              <b> ZENOU </b>
+            </h1>
+            <hr />
+            <h3 class="mt-5">Etudiante en développement web</h3>
+          </v-col>
+        </v-row>
+      </div>
+      <div class="section" id="section2">Some section</div>
+      <div class="section" id="section3">Some section</div>
+      <div class="section" id="section4">Some section</div>
+      <div class="section" id="section5">Some section</div>
+    </div>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
+  data: () => ({
+    //
+  }),
+};
+</script>
 <style>
+h1 {
+  margin-top: 0;
+  font-family: "Open Sans Condensed";
+  font-style: normal;
+  font-weight: 300;
+  font-size: 96px;
+  line-height: 100px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.5);
+  color: white;
+  letter-spacing: 0.5rem;
+}
+hr {
+  width: 50%;
+  height: 1px;
+  border: 0;
+  border-top: 1px solid black;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.5);
+}
+h3 {
+  font-family: "Raleway";
+  font-style: normal;
+  font-weight: 300;
+  font-size: 30px;
+  line-height: 44px;
+  color: #c1c1c1;
+  text-shadow: 0px 5px 4px rgba(0, 0, 0, 0.5);
+}
+#section1 {
+  background: linear-gradient(180deg, #0b525b 0%, #144552 100%);
+}
+#section2 {
+  background: linear-gradient(180deg, #144552 0%, #1b3a4b 100%);
+}
+#section3 {
+  background: linear-gradient(180deg, #1b3a4b 0%, #272640 100%);
+}
+#section4 {
+  background: linear-gradient(180deg, #272640 0%, #312244 100%);
+}
+#section5 {
+  background: linear-gradient(180deg, #312244 0%, #4d194d 100%);
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import vuetify from './plugins/vuetify';
+import fullpage from 'fullpage.js'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
+
+
+new fullpage('#fullpage', {
+  licenseKey: 'YOUR KEY HERE',
+  autoScrolling:true,
+	scrollHorizontally: true,
+  sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+});
+
