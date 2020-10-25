d6c6f9b383bb4a96cd8ef546e53c6fe0af7e056d
Emmanuelle Zenou <emmanuelle.zenou.rogala@gmail.com>
2020-10-25 23:15:42 +0100

    clouds moving

--- src/components/VCloud.vue
+++ src/components/VCloud.vue
@@ -27,21 +27,16 @@ export default {
     move() {
       let elem = document.querySelectorAll(".cloud");
       console.log("clouds", elem);
-      let rand;
-      let rand2;
+
       elem.forEach((cloud) => {
-        console.log(cloud)
-        rand = Math.floor(Math.random() * (30 - 20 + 1) + 20);
-        rand2 = Math.floor(Math.random() * (600 - 200 + 1) + 200);
+        let rand = Math.floor(Math.random() * (40 - 20 + 1) + 20);
         let speed = rand;
-        let pos = rand2;
+        let pos = cloud.style.right;
         setInterval(frame, speed);
         function frame() {
           if (pos == 2000) {
             pos = -300;
           } else {
-            // console.log("coucou");
-            // console.log(cloud.style.right)
             pos++;
             cloud.style.right = pos + "px";
           }
@@ -56,14 +51,16 @@ export default {
 #container {
   position: relative;
 }
-.cloud2 {
+#cloud2 {
   position: absolute;
   width: 300px;
   top: 100px;
+  right:20%;
 }
-.cloud1 {
+#cloud1 {
   position: absolute;
   width: 200px;
   top: 200px;
+    right:30%;
 }
 </style>
